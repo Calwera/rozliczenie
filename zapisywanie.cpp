27e293b7f48504b63464d916fb63ddc3263979c6
Calwera <41157106+Calwera@users.noreply.github.com>
2018-08-09 10:30:41 +0200

    Update zapisywanie.cpp

--- zapisywanie.cpp
+++ zapisywanie.cpp
@@ -1,3 +1,14 @@
 #include "zapisywanie.h"
+#include <fstream>
 #include <iostream>
 using namespace std;
+fstream plik;
+plik.open( "rozliczenie.txt", ios::in | ios::out );
+if( plik.good() == true )
+{
+    cout << "Uzyskano dostep do pliku!" << endl;
+    
+} else cout << "Dostep do pliku zostal zabroniony!" << endl;
+
+
+plik.close();
