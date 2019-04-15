87288edc5a8af6e13d9a44821f85c2c5f50b2982
Krzysztof <41157106+Calwera@users.noreply.github.com>
2019-04-15 11:50:23 +0200

    Update zapisywanie.cpp

--- zapisywanie.cpp
+++ zapisywanie.cpp
@@ -19,17 +19,23 @@ void zapisywanie::zapisz(string nazwa = dane)
         {
             cout << "Uzyskano dostep do pliku!" << endl;
     
-        } else cout << "Dostep do pliku zostal zabroniony!" << endl;
+        } else cout << "nie uzyskanu dostępu do pliku danych" << endl;
 
     plik << oper1.get_sumap() << " " <<  oper1.get_sumar() << " " << oper1.get_sumap() - oper1.get_sumar();
  
 plik.close();
 }
 
-void zapisywanie::odczytaj(string)
+void zapisywanie::odczytaj(string nazwa = dane)
 {
     fstream plik;
-    plik.open( dane, ios::out);
+    plik.open( dane, ios::in | ios::out );
+    if( plik.good() == true )
+        {
+            cout << "Uzyskano dostep do pliku!" << endl;
+    
+        } else cout << "nie uzyskanu dostępu do pliku danych" << endl;
+   
 
  
      plik.close();
